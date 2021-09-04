83e428a3b9059174cfa5375d4f1c4151b5f3d19f
David McKennirey <david.mckennirey@aon.com>
2021-09-04 13:51:56 -0700

    add gitignore and solution

---
+++ CreateRemoteThread/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CreateRemoteThread {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
